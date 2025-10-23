304bcac1fc0833e56a5736279b48de0f196904ef
darurat <akundarurat.1.2.3.4.5.6.7@gmail.com>
2025-10-23 13:24:42 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const buyButtons = document.querySelectorAll('.btn-buy');
-    buyButtons.forEach(btn => {
-        btn.addEventListener('click', () => {
-            alert('Produk ditambahkan ke keranjang! (Demo)');
-        });
-    });
-});
